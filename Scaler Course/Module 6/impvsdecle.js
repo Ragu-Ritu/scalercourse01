e2d63ecb870dfbb7e14f5465b9fc2571520ca18e
Ragu-Ritu <ragutest420@gmail.com>
2023-10-30 22:34:06 +0530

    renamed the main.js folder and added a imperative and declerative code

---
+++ Scaler Course/Module 6/impvsdecle.js
@@ -0,0 +1,16 @@
+//Imperative way of writing the code
+
+const a = 10;
+const asquared = a*a;
+
+if (asquared % 2 == 0){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+
+//Declerative way of writing the code
+
+const asquared1 = (b) => (b*b & 2 == 0 ? true : false); 
+console.log(asquared1(5));
