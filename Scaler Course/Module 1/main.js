643a1bdb13f6ec3bb4b40e02b05e9ceca6822293
Ragu-Ritu <ragutest420@gmail.com>
2023-10-31 21:53:36 +0530

    Added the extra 'console.log' types

--- Scaler Course/Module 1/main.js
+++ Scaler Course/Module 1/main.js
@@ -1,1 +1,26 @@
+//Print the value as a string
 console.log('Hello Im from script file');
+
+//Print the value as a number
+console.log(12);
+
+//Print the value as a decimal number
+console.log(12.005);
+
+//Print the value as a array format
+console.log([1, 2, 3, 4, 5]);
+
+//Print the value as a json format
+console.log({fname: 'Ragu', age: 25});
+
+//Print the value as a json format in tabel
+console.table({fname: 'Ragu', age: 25});
+
+//Print the value as a error string
+console.error('you need to fill the mandatory field');
+
+//Print the value as a warn string
+console.error(`I'm warning you`);
+
+//to clear the console we can use this command
+console.clear();
