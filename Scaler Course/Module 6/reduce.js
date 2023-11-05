55d5f5dd653fb76316036de9ce8d950d6c623201
Ragu-Ritu <ragutest420@gmail.com>
2023-11-05 23:34:09 +0530

    modified the code

--- Scaler Course/Module 6/reduce.js
+++ Scaler Course/Module 6/reduce.js
@@ -8,8 +8,21 @@ let a = [1, 2, 3, 4, 5];
 
 let b = 1;
 
-for(let i = 0; 1<a.length; i++){
+for(let i = 0; i<a.length; i++){
     b = b+a[i];
 }
 
 console.log(b);
+
+// Declerative way of writing the code
+
+// using reduce and normal function method
+
+let a1 = [1, 2, 3, 4, 5];
+
+let result = a1.reduce(function(accumulator, value){
+    let sum1 = accumulator+value;
+    return sum1;
+} , 0)
+
+console.log('The sum of an array using the normal function method is: ' + sum1);
