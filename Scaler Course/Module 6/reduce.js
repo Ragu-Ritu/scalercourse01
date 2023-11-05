f2204c65374d41a20277bc91379d8c136aba77dc
Ragu-Ritu <ragutest420@gmail.com>
2023-11-05 23:17:47 +0530

    Created a new file and named the file as 'reduce.js'

---
+++ Scaler Course/Module 6/reduce.js
@@ -0,0 +1,15 @@
+// Reduce method
+
+// How to print the sum of a array
+
+//Imperative way of code
+
+let a = [1, 2, 3, 4, 5];
+
+let b = 1;
+
+for(let i = 0; 1<a.length; i++){
+    b = b+a[i];
+}
+
+console.log(b);
